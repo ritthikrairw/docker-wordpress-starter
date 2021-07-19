88e027c192e72f9f9920f56e7c8c5bab6b77a7ce
Ritthikrai W <ritthikrai.rw@gmail.com>
2021-07-20 03:13:09 +0700

    ✨ Update:  checking the mhsendmail and mailhog.ini existing

--- system/scripts/mailhog-init.sh
+++ system/scripts/mailhog-init.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
-echo "mhsendmail installing..."
+echo "checking the mhsendmail existing..."
+if [ ! -f /usr/local/bin/mhsendmail ]; then
+    echo "mhsendmail does not exist"
+    echo "start installing a mhsendmail..."
 
-# Install mhsendmail for Mailhog
-curl --location --output /usr/local/bin/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
-chmod +x /usr/local/bin/mhsendmail
+    # Install mhsendmail for Mailhog
+    curl --location --output /usr/local/bin/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
+    chmod +x /usr/local/bin/mhsendmail
 
-echo "config the mailhog..."
-echo 'sendmail_path="/usr/local/bin/mhsendmail --smtp-addr=mailhog:1025 --from=no-reply@mailhog.local"' > /usr/local/etc/php/conf.d/mailhog.ini
+    echo "install mhsendmail successful"
 
-echo "done."
+else
+    echo "mhsendmail is existed"
+fi
+
+echo "checking the mailhog.ini..."
+if [ ! -f /usr/local/etc/php/conf.d/mailhog.ini ]; then
+    echo "start config the mailhog.ini..."
+    echo 'sendmail_path="/usr/local/bin/mhsendmail --smtp-addr=mailhog:1025 --from=no-reply@mailhog.local"' > /usr/local/etc/php/conf.d/mailhog.ini
+    echo "config the mailhog.ini successful"
+else
+    echo "mailhog.ini already configured"
+fi
 
 # execute
 exec "php-fpm"
