e04051d848b5326b71b19983a7e202f5d527ab47
Ritthikrai W <ritthikrai.rw@gmail.com>
2021-11-29 09:23:45 +0700

    update deploy script

--- system/deploy.sh
+++ system/deploy.sh
@@ -5,7 +5,7 @@
 #--------------------------------------------------------------------------------------------
 
 # Set variables
-DIR_PATH=${__DIR_PATH}
+DIR_PATH=$__DIR_PATH
 PUBLIC_PATH=$DIR_PATH/public_html
 REVISION_NAME=$(date +"%Y%m%d%H%M%S")
 REVISION_PATH=$DIR_PATH/revisions/$REVISION_NAME
@@ -25,33 +25,40 @@ mkdir $REVISION_PATH
 cp $DIR_PATH/deploy/deploy-build.tar.gz $REVISION_PATH
 cd $REVISION_PATH
 tar -xzvf deploy-build.tar.gz
-rm deploy-build.tar.gz
+sudo rm deploy-build.tar.gz
 
 # Check exist files
 FILE=$REVISION_PATH/.htaccess
 if [ -f "$FILE" ]; then
-    rm -rf $FILE
+    sudo rm -rf $FILE
 fi
 
 FILE=$REVISION_PATH/wp-config.php
 if [ -f "$FILE" ]; then
-    rm -rf $FILE
+    sudo rm -rf $FILE
 fi
 
 DIR=$REVISION_PATH/wp-content/uploads
 if [ -d "$DIR" ]; then
-    rm -rf $DIR
+    sudo rm -rf $DIR
 fi
 
 # Symlinks
-rm -rf $PUBLIC_PATH
+sudo rm -rf $PUBLIC_PATH
 ln -s $REVISION_PATH $PUBLIC_PATH
 ln -s $SHARED_PATH/uploads $PUBLIC_PATH/wp-content
 ln -s $SHARED_PATH/.htaccess $PUBLIC_PATH/.htaccess
 ln -s $SHARED_PATH/wp-config.php $PUBLIC_PATH/wp-config.php
 
+# Set chown and chmod
+USER=$__USER
+GROUP=$__GROUP
+
+sudo chown -R $USER:$GROUP $PUBLIC_PATH
+sudo chmod 755 $PUBLIC_PATH
+
 # Unset variables and remove files
-rm -rf $DIR_PATH/deploy
+sudo rm -rf $DIR_PATH/deploy
 unset DIR_PATH
 unset PUBLIC_PATH
 unset REVISION_NAME
