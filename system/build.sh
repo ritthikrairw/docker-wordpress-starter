555f48c5a9ea6ad01d31087936823d5c32ce2ce6
Ritthikrai W <ritthikrai.rw@gmail.com>
2021-12-16 22:28:03 +0700

    update scripts

--- system/build.sh
+++ system/build.sh
@@ -7,15 +7,22 @@
 
 # Remove all *.gz files
 
+echo "start remove old package..."
+
 rm -rf ${WORKSPACE}/*.gz
 rm -rf ${WORKSPACE}/system/*.gz
 
+echo "remove old package success."
+
+echo "current workspace directory"
 echo ${WORKSPACE}
+
+echo "start build package"
 
 # Build Website Package
 FILE_NAME=deploy-build.tar.gz \
     ;
-tar -zcvf \
+tar -zcf \
     $FILE_NAME \
     --exclude="*.git*" \
     --exclude="*.htaccess*" \
