c54e64129f8d6f9557db127d91e8604e5a29d048
Ritthikrai W <ritthikrai.rw@gmail.com>
2022-01-11 16:34:18 +0700

    update: optimized config

--- system/docker/wordpress/install.sh
+++ system/docker/wordpress/install.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# Install mhsendmail
-if [ ! -f /usr/local/bin/mhsendmail ]; then
-    echo "####### start installing a mhsendmail..."
-
-    # Install mhsendmail for Mailhog
-    curl --location --output /usr/local/bin/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
-    chmod +x /usr/local/bin/mhsendmail
-
-    echo "####### install mhsendmail successful"
-fi
-
-if [ ! -f /usr/local/etc/php/conf.d/mailhog.ini ]; then
-    echo "####### start config the mailhog.ini..."
-    echo 'sendmail_path="/usr/local/bin/mhsendmail --smtp-addr=mailhog:1025 --from=no-reply@mailhog.local"' >/usr/local/etc/php/conf.d/mailhog.ini
-    echo "####### config the mailhog.ini successful"
-fi
 
 # Install WP-CLI
 if [ ! -f /usr/local/bin/wp ]; then
