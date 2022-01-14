dc17f7d102d47c594b8d83ed2195958735fdd640
Ritthikrai W <ritthikrai.rw@gmail.com>
2022-01-14 08:45:04 +0700

    remove app files

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -1,133 +1,207 @@
 <?php
+
 /**
+
  * The base configuration for WordPress
+
  *
+
  * The wp-config.php creation script uses this file during the installation.
+
  * You don't have to use the web site, you can copy this file to "wp-config.php"
+
  * and fill in the values.
+
  *
+
  * This file contains the following configurations:
+
  *
+
  * * MySQL settings
+
  * * Secret keys
+
  * * Database table prefix
+
  * * ABSPATH
+
  *
- * This has been slightly modified (to read environment variables) for use in Docker.
- *
+
  * @link https://wordpress.org/support/article/editing-wp-config-php/
+
  *
+
  * @package WordPress
+
  */
 
-// IMPORTANT: this file needs to stay in-sync with https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
-// (it gets parsed by the upstream wizard in https://github.com/WordPress/WordPress/blob/f27cb65e1ef25d11b535695a660e7282b98eb742/wp-admin/setup-config.php#L356-L392)
-
-// a helper function to lookup "env_FILE", "env", then fallback
-if (!function_exists('getenv_docker')) {
-	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
-	function getenv_docker($env, $default) {
-		if ($fileEnv = getenv($env . '_FILE')) {
-			return rtrim(file_get_contents($fileEnv), "\r\n");
-		}
-		else if (($val = getenv($env)) !== false) {
-			return $val;
-		}
-		else {
-			return $default;
-		}
-	}
-}
 
 // ** MySQL settings - You can get this info from your web host ** //
+
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
+
+define( 'DB_NAME', 'wordpress' );
+
 
 /** MySQL database username */
-define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
+
+define( 'DB_USER', 'wordpress' );
+
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
 
-/**
- * Docker image fallback values above are sourced from the official WordPress installation wizard:
- * https://github.com/WordPress/WordPress/blob/f9cc35ebad82753e9c86de322ea5c76a9001c7e2/wp-admin/setup-config.php#L216-L230
- * (However, using "example username" and "example password" in your database is strongly discouraged.  Please use strong, random credentials!)
- */
+define( 'DB_PASSWORD', '123456' );
+
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv_docker('WORDPRESS_DB_HOST', 'mysql') );
+
+define( 'DB_HOST', 'mariadb:3306' );
+
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
+
+define( 'DB_CHARSET', 'utf8' );
+
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
+
+define( 'DB_COLLATE', '' );
+
 
 /**#@+
+
  * Authentication unique keys and salts.
+
  *
+
  * Change these to different unique phrases! You can generate these using
+
  * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+
  *
+
  * You can change these at any point in time to invalidate all existing cookies.
+
  * This will force all users to have to log in again.
+
  *
+
  * @since 2.6.0
+
  */
-define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         'fd461a300c610925f87524bc7f4a5baa2eea4164') );
-define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  '794590a84ca99473893a4a68285271ffb6881129') );
-define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    '59a9fdf63d442621448d9a91b8cfb705ff921c8f') );
-define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        '832dabc99be0bb8e94cc198deee6439fcaca1432') );
-define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        '781d6bdabc517ce7d73ef4759f0ad2920ef61a78') );
-define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', 'cfb1a84bdc38dda2dbe4efd172ca9a7441e032ae') );
-define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   'fe7a45a81c0bf90d091768931154f559df52c38f') );
-define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       'e84bb048c22b5b290231167f56636f488d7b695a') );
-// (See also https://wordpress.stackexchange.com/a/152905/199287)
+
+define( 'AUTH_KEY',         '5kDAhae<N);6!!V`wi=/<KzP/AqEk$ig|;WJ>I5/B%pW5<aaY5XsxCeN:s(w>bhz' );
+
+define( 'SECURE_AUTH_KEY',  'XH$SSV}_q)itM251rJLzlUhFYp.o HBWIBU;<&46<k@9KFV~xEAhT;pR@Z,.lBhP' );
+
+define( 'LOGGED_IN_KEY',    '!J6|Ah~!vu;@:kkh5*Hpd>ohyq_E-e{VD#M:|i@i{ki$Q6 [H$M~z+[j.1rs2kut' );
+
+define( 'NONCE_KEY',        '(>`>68g#d/q[]6y}e )heHGBD {&}TXLY=W%, TdggO<0kwDq6c1FF/|N#rbgkGA' );
+
+define( 'AUTH_SALT',        '+i#wk6xvzcS +s<|MxsWVzAPJdchygtam3S Vwp+_EVg{ZE0[CA)q[Pev<vVcQ`E' );
+
+define( 'SECURE_AUTH_SALT', '4J@h:]c&Lxn |:xl1fOk)FB*DVClxXCznX4ds-%+Z@4ITYFaAn>`KFWnPR(,QDx5' );
+
+define( 'LOGGED_IN_SALT',   'K9f/mRY&]Vhy$9)g}lx3CmPt#B^{< UU|pgQ}KmxvHgmRqq-bzHSJp$7=5JueD0q' );
+
+define( 'NONCE_SALT',       '.E9ZCXYw}3CCpW#( FyaSc{2Q..Iert&5}Wl=Xm1&|H2XcL0sH,Ee4RTPlKgTp2Y' );
+
 
 /**#@-*/
 
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
 
 /**
- * For developers: WordPress debugging mode.
+
+ * WordPress database table prefix.
+
  *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
+
+ * You can have multiple installations in one database if you give each
+
+ * a unique prefix. Only numbers, letters, and underscores please!
+
  */
-define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
+
+$table_prefix = 'wp_';
+
+
+/**
+
+ * For developers: WordPress debugging mode.
+
+ *
+
+ * Change this to true to enable the display of notices during development.
+
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+
+ * in their development environments.
+
+ *
+
+ * For information on other constants that can be used for debugging,
+
+ * visit the documentation.
+
+ *
+
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+
+ */
+
+define( 'WP_DEBUG', false );
+
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-// (we include this by default because reverse proxying is extremely common in container environments)
 
-if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
-	eval($configExtra);
+
+
+define( 'FS_METHOD', 'direct' );
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+ */
+if ( defined( 'WP_CLI' ) ) {
+	$_SERVER['HTTP_HOST'] = '127.0.0.1';
 }
 
+define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/' );
+define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/' );
+define( 'WP_AUTO_UPDATE_CORE', false );
 /* That's all, stop editing! Happy publishing. */
 
+
 /** Absolute path to the WordPress directory. */
+
 if ( ! defined( 'ABSPATH' ) ) {
+
 	define( 'ABSPATH', __DIR__ . '/' );
+
 }
 
+
 /** Sets up WordPress vars and included files. */
+
 require_once ABSPATH . 'wp-settings.php';
+
+/**
+ * Disable pingback.ping xmlrpc method to prevent WordPress from participating in DDoS attacks
+ * More info at: https://docs.bitnami.com/general/apps/wordpress/troubleshooting/xmlrpc-and-pingback/
+ */
+if ( !defined( 'WP_CLI' ) ) {
+	// remove x-pingback HTTP header
+	add_filter("wp_headers", function($headers) {
+		unset($headers["X-Pingback"]);
+		return $headers;
+	});
+	// disable pingbacks
+	add_filter( "xmlrpc_methods", function( $methods ) {
+		unset( $methods["pingback.ping"] );
+		return $methods;
+	});
+}
