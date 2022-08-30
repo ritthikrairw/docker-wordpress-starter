351b34265218c66a9aa3b198e0dfa5846da54109
Ritthikrai (Champ) Wiengchai <ritthikrai.rw@gmail.com>
2022-08-30 20:53:32 +0700

    add script to install composer and wp cli

---
+++ system/scripts/wp-cli.sh
@@ -0,0 +1,6 @@
+# install wp cli script
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
